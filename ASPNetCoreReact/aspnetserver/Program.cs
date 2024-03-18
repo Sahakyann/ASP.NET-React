54827b57fc79fe23c15612b302500a2d3d0b61ec
Hrach Sahakyan <sahakyanhrach.b@gmail.com>
2024-03-19 00:47:29 +0400

    Both server side and front end done. Implemented functionality to handle requests async in program.cs file and created a simple react front end to send the requests
    (Commiting again as the the previous commits have not been going through)

--- ASPNetCoreReact/aspnetserver/Program.cs
+++ ASPNetCoreReact/aspnetserver/Program.cs
@@ -1,20 +1,102 @@
+using aspnetserver.Data;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+        });
+
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Posts", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUIOptions.DocumentTitle = "Posts";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API");
+    swaggerUIOptions.RoutePrefix = string.Empty;
+});
+
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
+
+app.MapGet("/get-all-posts",async () => await PostsRepository.GetPostsAsync()).WithTags("Posts Endpoints");
+
+app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
+{
+    Post postToReturn = await PostsRepository.GetPostByIdAsync(postId);
+
+    if (postToReturn != null)
+    {
+        return Results.Ok(postToReturn);
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapPost("/create-post", async (Post postToCreate) =>
+{
+    bool createSuccessful = await PostsRepository.CreatePostAsync(postToCreate);
+
+    if (createSuccessful)
+    {
+        return Results.Ok("Create successful.");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapPut("/update-post", async (Post postToUpdate) =>
+{
+    bool updateSuccessful = await PostsRepository.UpdatePostAsync(postToUpdate);
+
+    if (updateSuccessful)
+    {
+        return Results.Ok("Update successful.");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
+{
+    bool deleteSuccessful = await PostsRepository.DeletePostAsync(postId);
+
+    if (deleteSuccessful)
+    {
+        return Results.Ok("Delete successful.");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
 
 app.Run();
 
